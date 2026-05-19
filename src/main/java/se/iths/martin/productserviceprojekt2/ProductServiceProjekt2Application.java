d5aa31bf3173da777d117278c382185d5d1bc5b4
MartinGruter <Martin.Grter@iths.se>
2026-05-19 10:06:09 +0200

    Init

---
+++ src/main/java/se/iths/martin/productserviceprojekt2/ProductServiceProjekt2Application.java
@@ -0,0 +1,13 @@
+package se.iths.martin.productserviceprojekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceProjekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceProjekt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/martin/productserviceprojekt2/ProductServiceProjekt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.martin.productserviceprojekt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceProjekt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
